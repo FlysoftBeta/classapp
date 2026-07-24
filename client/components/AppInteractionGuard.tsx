aae9d3fa5721a23b484a4de8bc8bdda66bf7477c
Flysoft <flysoftbeta@outlook.com>
2026-07-24 23:31:40 +0800

    scrolling performance issues!

--- client/components/AppInteractionGuard.tsx
+++ client/components/AppInteractionGuard.tsx
@@ -6,22 +6,14 @@ export default function AppInteractionGuard() {
       event.preventDefault();
     };
 
-    const preventCtrlWheelZoom = (event: WheelEvent) => {
-      if (event.ctrlKey) event.preventDefault();
-    };
-
     document.addEventListener("gesturestart", preventGesture);
     document.addEventListener("gesturechange", preventGesture);
     document.addEventListener("gestureend", preventGesture);
-    document.addEventListener("wheel", preventCtrlWheelZoom, {
-      passive: false,
-    });
 
     return () => {
       document.removeEventListener("gesturestart", preventGesture);
       document.removeEventListener("gesturechange", preventGesture);
       document.removeEventListener("gestureend", preventGesture);
-      document.removeEventListener("wheel", preventCtrlWheelZoom);
     };
   }, []);
 
